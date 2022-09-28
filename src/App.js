764c68b6d2d49379b76c8532ed2c7c29c4a2545a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-28 16:45:16 +0530

    initial commit of tours app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+function App() {
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
