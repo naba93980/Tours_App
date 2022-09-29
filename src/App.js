47fa99f924477d7d9c8126f2b0f1d21b3c4dfba3
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-29 07:18:55 +0530

    app component once rendered fetches tour data and passes data to child tours component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
-}
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
 
-export default App
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>refresh</button>
+        </div>
+      </main>
+    );
+  }
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />
+      </main>
+    );
+  }
+}
+export default App;
